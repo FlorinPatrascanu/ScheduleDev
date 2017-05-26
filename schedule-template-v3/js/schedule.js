dcb32e417a484a7059efffbc51e66f32447e02d1
Daniel Voicu <daniel.voicu@brandfusion.ro>
2017-05-26 19:37:29 +0300

    update
    update

--- schedule-template-v3/js/schedule.js
+++ schedule-template-v3/js/schedule.js
@@ -1,3 +1,10 @@
+var scheduleConfig = {
+	timeframe: [8,20],
+	gridHeightCell: 40,
+	gridWidthCell: 300,
+}
+
+
 function loading(arg) {
 	if(arg == true) {
 		$('#loading').removeClass('fadeOut').removeClass('fadeIn');
@@ -18,9 +25,15 @@ function conferenceArray(data){
 	var conference = _.filter(data , function(obj){
 		return obj.name === "Conference Path";
 	});
-
+  var conferencePathExists = _.size(conference) > 0 ? true : false;
+  var conferencePathId = "";
+  if (conferencePathExists == true) {
+  	conferencePathId = conference[0].id;
+  } else {
+  	conferencePathId = "CONFERENCEPATHDOESNOTEXIST";
+  }
 	var conferencePathArrayRaw = _.filter(data, function(obj){
-		return obj.parentid === conference[0].id;
+		return obj.parentid === conferencePathId;
 	})
 	var conferencePathFinal = _.reduce(conferencePathArrayRaw, function(result, value, key){
 		var obj = {};
@@ -33,6 +46,11 @@ function conferenceArray(data){
 
 	return conferencePathFinal;
 }
+function resetWhenDataIsInvalid() {
+	console.error("Error loading API");
+		$('#event-select-wrapper').show();
+		loading(false);
+}
 function GetAPIData(link){
 	var deferred = $.Deferred();
 	$.ajax({
@@ -40,34 +58,38 @@ function GetAPIData(link){
 		type: 'GET',
 		dataType: 'jsonp'
 	})
-	.done(function(data){
+	.done(function(data){		
 		deferred.resolve(data);
 	})
 	.fail(function(error) {
-		deferred.reject(error);
-		console.log("error");
+		console.log("error API:", link);
+		deferred.reject();		
 	});
 	return deferred.promise();
 }
 function getData(event){
 	var deferred = $.Deferred();
-	var event = "nav2016us";
-	var daysUrl = 'http://api.eventpoint.com/2.3/program/days?code='+event+'&apikey=a325a4c2a3ed435eb1eb9e8f0dddeb03';
-	var topicsUrl = 'http://api.eventpoint.com/2.3/program/topics?code='+event+'&apikey=a325a4c2a3ed435eb1eb9e8f0dddeb03';
-	var categoriesUrl = 'https://api.eventpoint.com/2.3/program/categories?code='+event+'&apikey=a325a4c2a3ed435eb1eb9e8f0dddeb03';
+	var eventId = event.value;
+	var eventApi = event.apiKey;
+	var eventTimeAdjust = parseFloat(event.timeAdjust);
+	var daysUrl = 'http://api.eventpoint.com/2.3/program/days?code='+eventId+'&apikey=' + eventApi;
+	var topicsUrl = 'http://api.eventpoint.com/2.3/program/topics?code='+eventId+'&apikey=' + eventApi;
+	var categoriesUrl = 'https://api.eventpoint.com/2.3/program/categories?code='+eventId+'&apikey=' + eventApi;	
 	loading(true);
 	$.when(GetAPIData(daysUrl),GetAPIData(topicsUrl),GetAPIData(categoriesUrl)).then(function(v1,v2,v3){
 		var conferencePathArray = conferenceArray(v3);
 		var scheduleData = _.reduce(v1, function(result, value, key){
 			var date = value.date;
-			date = date.split('T')[0];
+			// date = date.split('T')[0];
+			date = moment(date).format("YYYY-MM-DD");
 
 			var obj = {};
 			obj.date = date;
 			obj.topics = _.chain(v2.results)
-										.filter(function(o){
-											// console.log(o);
-											return (o.start).split("T")[0] == date;
+										.filter(function(o){											
+											var dateAdjusted = "";											
+											dateAdjusted = moment(o.start).utcOffset(eventTimeAdjust).format("YYYY-MM-DD");										
+											return dateAdjusted == date;
 										})
 										.reduce(function(result, value, key){
 											var obj = {};
@@ -77,9 +99,10 @@ function getData(event){
 											obj.publishingStatus = value.publishingstatus;
 											obj.room = value.room;
 											obj.categoryids = value.categoryids;
-											obj.start = (value.start).split('T')[1].substring(0 , 5);
-											obj.finish = (value.finish).split('T')[1].substring(0 , 5);
-											// _.map(value.categoryids, function(value){
+											obj.start = moment(value.start).utcOffset(eventTimeAdjust).format("HH:mm");
+											obj.finish = moment(value.finish).utcOffset(eventTimeAdjust).format("HH:mm");		
+											// console.log("timeadjust", Math.abs(eventTimeAdjust), "times:",obj.start, "timesoriginal", value.start);									
+										
 											obj.conferencePath = _.filter(conferencePathArray, function(object){
 												return _.includes(value.categoryids, object.id)
 											});
@@ -88,34 +111,46 @@ function getData(event){
 											return result;
 
 										},[])
+										.sortBy("start")
 										.value();
 			result.push(obj);
 			return result;
 		},[]);
 
-
+		$("#schedule").empty().removeClass("animated fadeOut"); // CLEARS SCHEDULE TABLE
 		deferred.resolve(scheduleData);
+	},function() {
+		//error getting API => select again
+		resetWhenDataIsInvalid();
+		deferred.reject();
+    
 	});
 	return deferred.promise();
 }
-function buildEventSelect(){
-	var eventSelect = [];
-	$('#event-select option').each(function(){
-		var value = $(this).attr("value") == "" ? "" : $(this).attr("value");
-		var name = $(this).html();
-		eventSelect.push({"value": value, "name": name});
-	});
-	// eventSelect.shift();
+function buildEventSelect(eventData){		
 	var output = '<select id="header-event-select" class="form-control">';
-	_.map(eventSelect, function(o){
-		if(o.value == "") {
-			output += '<option value="'+o.value+'" disabled selected>'+o.name+'</option>';
-		} else {
-			output += '<option value="'+o.value+'">'+o.name+'</option>';
-		}
+	output += '<option value="" disabled selected>Pick an event</option>';
+	_.map(eventData, function(o){
+		// if(o.value == "") {
+		// 	output += '<option value="'+o.value+'" disabled selected>'+o.name+'</option>';
+		// } else {
+			output += '<option value="'+o.id+'">'+o.name+'</option>';
+		// }
 
 	});
 	output += '</select>';
+	return output;
+}
+function buildEventSelectPrimary(eventData){		
+	var output = "";
+	output += '<div id="event-select-wrapper">';
+	output += '<select id="event-select" class="form-control">';
+	output += '<option value="" disabled selected>Pick an event</option>';
+	_.map(eventData, function(o){	
+			output += '<option value="'+o.id+'">'+o.name+'</option>';
+	});
+	output += '</select>';
+	output += '</div>';
 	return output;
 }
 function buildDaySelect(data){
@@ -130,68 +165,125 @@ function buildDaySelect(data){
 			output += '<option value="'+val+'">'+val+'</option>';
 	});
 	output += '</select>';
-	return output;
-}
+	return output;}
+
 function buildConferencePathSelect(data){
 	var conferencePathArray = _.chain(data)
 														.reduce(function(result, value, key){
-															// var array = [];
-															// if (_.size(value.conferencePath) > 0) {
-															// 		result.concat(value.conferancePath);
-															// }
-															// return result;
+															var topicConferenceArray = _.reduce(value.topics, function(result2, value2, key2){
+																// console.log(value2.conferencePath);																
+																result2 = _.concat(result2, value2.conferencePath);															
+																return result2;
+															},[]);		
+															// console.log("topicConferenceArray",topicConferenceArray);		
+															result = _.concat(result, topicConferenceArray);										
+															return result;
 														},[])
-														.uniq()
-														.value();
-	console.log(conferencePathArray);
-	// var output = '<select id="header-day-select" class="form-control">';
-	// // output += '<option value="" disabled seleted>Pick a date</option>';
-	// _.map(days, function(val){
-	// 		output += '<option value="'+val+'">'+val+'</option>';
-	// });
-	// output += '</select>';
-	// return output;
+														.uniqBy("id")
+														.sortBy(function(o){return o.name})
+														.value();	
+	var output = '<select id="header-conference-select" class="form-control">';
+	output += '<option value="" seleted>Pick a conference path</option>';
+	_.map(conferencePathArray, function(o){
+			output += '<option value="'+o.id+'">'+o.name+'</option>';
+	});
+	output += '</select>';
+	return output;
 }
-function renderHeader(data) {
+function renderHeader(data, event) {
 	var output = '<div id="schedule-header" class="form-inline animated fadeIn">';
 	output += '<i class="fa fa-calendar" aria-hidden="true"></i>';
-	output += buildEventSelect();
+	output += buildEventSelect(eventData);
 	output += buildDaySelect(data);
+	output += buildConferencePathSelect(data);
 	output += '</div>';
 	$("#schedule").prepend(output);
-	$("#header-event-select").val($("#event-select").val());
+	// console.log("event",event);
+	$("#header-event-select").val(event.id);
+}
+function renderGrid(timeframe){
+	var output = '<div id="grid">';
+	var timeframeArray = []
+	for (var i = timeframe[0]; i < timeframe[1]; i++) {
+		timeframeArray.push(i);
+	}
+	// console.log(timeframeArray);
+	_.map(timeframeArray, function(val){
+		output += '<div class="hours">';
+		var intervals = ["00","15","30","45"];
+		_.map(intervals, function(inter){
+			output += '<div class="intervals"><span>'+val+':'+inter+'</span><span>'+val+':'+inter+'</span></div>'
+		});
+		output += '</div>';
+	});
+	output += '</div>';
+	// console.log(output);
+
+	$("#schedule").append(output);
+	$('#schedule .intervals').height(scheduleConfig.gridHeightCell).css("lineHeight", scheduleConfig.gridHeightCell + 'px');
+}
+function renderCard(o){
+	output = "";
+	output += '<div style="width:'+scheduleConfig.gridWidthCell+'px;">';
+	output += '<p>'+o.title+'<p>';
+	output += '</div>';
+	return output;
 }
 function renderTable(data) {
-	console.log(data);
+	console.log(data.topics);
+	output = "";
+	output += '<div id="event">';
+	_.map(data.topics, function(o){
+		output += renderCard(o);
+	});
+	output += '</div>';
+	$('#schedule').append(output);
 }
-function Schedule(data){
-
-
-	 renderHeader(data);
-	//  buildConferencePathSelect(data);
-
-
-	 $('#header-day-select').on("change", function(){
-		 var date = $(this).val();
-		 var dataFilteredByDate = _.filter(data, function(o){
-			 return o.date == date;
-		 });
-		 renderTable(dataFilteredByDate);
-	 });
+function Schedule(data, event){	
+	renderHeader(data, event);
+	renderGrid(scheduleConfig.timeframe);
+	renderTable(data[0]);
+ $('body').on("change", "#header-day-select", function(){
+	 var value = $(this).val();
+	 var dataFilteredByDate = _.filter(data, function(o){	 
+		 return o.date == value;
+	 });	
+	 renderTable(dataFilteredByDate[0]);
+ });
 		//Run
 		// console.log("schedule",data);
-		loading(false);
+	loading(false);
 
 }
 
 
 
 $(function(){
-	$('#event-select').on("change", function(){
-		var event = $(this).val();
+	//init first select
+	$("#schedule").before(buildEventSelectPrimary(eventData));
+	//on first select change
+	$('body').on("change", "#event-select", function(){
+		var value = $(this).val();
 		$('#event-select-wrapper').hide();
+		var eventSelectedArray = _.filter(eventData, function(o){
+			return o.id == value;
+		});
+		var event = eventSelectedArray[0];		
 		$.when(getData(event)).then(function(data){
-			Schedule(data);
+			Schedule(data, event); //event = need to send event information also for select to have the proper event value already selected;
+		});
+
+	});
+	//application event change
+	$('body').on("change", "#header-event-select", function(){
+		var value = $(this).val();
+		$('#schedule').addClass("animated fadeOut");		
+		var eventSelectedArray = _.filter(eventData, function(o){
+			return o.id == value;
+		});
+		var event = eventSelectedArray[0];		
+		$.when(getData(event)).then(function(data){
+			Schedule(data, event); //event = need to send event information also for select to have the proper event value already selected;
 		});
 
 	});
